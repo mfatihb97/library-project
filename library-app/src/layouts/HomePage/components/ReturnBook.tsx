92b38d9cf1c03810247ef6896e87481b9a707673
mfatihb97 <mfatihbatur@gmail.com>
2024-08-17 10:16:13 +0300

    update

--- library-app/src/layouts/HomePage/components/ReturnBook.tsx
+++ library-app/src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,17 +1,27 @@
+import BookModel from '../../../models/BookModel';
 import bookImage from './../../../Images/BooksImages/book-luv2code-1000.png';
 
-export const ReturnBook = () => {
+export const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
     return (
         <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
             <div className='text-center'>
-                <img
-                    src={bookImage}
-                    alt="book"
-                    width='151'
-                    height='233'
-                />
-                <h6 className='mt-2'>Book</h6>
-                <p>Mufaba</p>
+                {props.book.img ?
+                    <img
+                        src={props.book.img}
+                        alt="book"
+                        width='151'
+                        height='233'
+                    />
+                    :
+                    <img
+                        src={bookImage}
+                        alt="book"
+                        width='151'
+                        height='233'
+                    />
+                }
+                <h6 className='mt-2'>{props.book.title}</h6>
+                <p>{props.book.author}</p>
                 <a className='btn main-color text-white' href="#">Reserve</a>
             </div>
         </div>
